e52c2e70a02126157679de6ed3c3411b54f9c15e
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-09-27 11:38:41 +0600

    Advanced Join Query With Problem

--- Resources/33_Joining_Table.sql
+++ Resources/33_Joining_Table.sql
@@ -1,3 +1,8 @@
 -- JOINING TABLE WITHOUT JOIN KEYWORD
 -- SYNTAX select columns of both table select table names(comma separated) base contidion
 SELECT *,DEPARTMENT_NAME FROM employees,departments WHERE employees.DEPARTMENT_ID=departments.DEPARTMENT_ID;
+-- Join Query With JOIN Keyword
+
+select First_name,department_name from employees join departments on employees.department_id=departments.department_id;  
+
+select First_name,department_name from employees join departments using(department_id);  
---
+++ Resources/34_Advanced_Joining_Query.sql
@@ -0,0 +1,27 @@
+-- Show every employees manager id from system database
+select First_name,manager_id from employees;
+
+-- Show every employees manager name from system database
+select employee.First_name,manager.First_name from employees as employee join employees as manager on employee.employee_id=manager.manager_id;
+
+-- Show the department name which has no employee from system database
+select departments.department_name
+from departments
+left join employees 
+on departments.department_id=employees.department_id
+where employee_id is null;
+
+-- Show employees first_name, salary and difference salary between average salary of his department from system database
+select employees.first_name,employees.salary,(select avg(salary) from employees where department_id=departments.department_id)
+from employees
+join departments
+on employees.department_id=departments.department_id;
+
+-- Show the departments name where the salary is greater than a certain amount.
+select department_name
+from departments
+join employees
+on departments.department_id=employees.department_id
+group by department_name
+having avg(salary)>5000;
+
